514dae0144f7d0f3dde1ffa7fd890084bfb75bb3
mariiakuzka <mariiakuzka@gmail.com>
2021-09-25 13:19:05 +0300

    pagination for api and buttons next and previous

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
     def index
-        @query = params[:query]
+        #@query = params[:query]
         #params = {query: "gay", commit: "Search"}
-      
-        search_results = BookFetcher.call(@query)
+
+        #@start_index = params[:start_index]
+
+        search_results = BookFetcher.call(params[:query], params[:start_index])
 
         @books_data = search_results.to_a
         
--- app/services/book_fetcher.rb
+++ app/services/book_fetcher.rb
@@ -2,12 +2,13 @@ class BookFetcher
     BOOKS_API_DOMAIN      = 'https://www.googleapis.com'.freeze
     BOOKS_API_COMMON_PATH = '/books/v1'.freeze
 
-    def self.call(query)
-        new(query)
+    def self.call(query, start_index)
+        new(query, start_index)
     end
      
-    def initialize(query)
-        @query=query
+    def initialize(query, start_index)
+        @query       = query
+        @start_index = start_index.to_i
     end
 
     def to_a
@@ -17,11 +18,11 @@ class BookFetcher
 
 
     def query_search
-        Faraday.get("#{common_path}/volumes?q=#{@query}")
+        Faraday.get("#{common_path}/volumes?q=#{@query}&startIndex=#{@start_index}")
     end
 
-   
     private
+
     def common_path
         BOOKS_API_DOMAIN + BOOKS_API_COMMON_PATH
     end 
