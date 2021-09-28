8e2d39845561c19a474fb803744fa21124b3a983
mariiakuzka <mariiakuzka@gmail.com>
2021-09-28 21:27:21 +0300

    comments feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,24 @@ class CommentsController < ApplicationController
     
     
     def create
+     Comment.create(comments_params)
+     flash[:notice] = "ваш комментарий успешно добавлен"
+     redirect_to favorite_book_path(comments_params[:favorite_book_id])
+     
     end    
     
     def reply
     end
 
     def delete
+        
     end
-    
+  
+    private
+
+    def comments_params
+      params.require(:comment).permit(:content, :user_id, :favorite_book_id, :parent_id)
+    end
+
     
 end
